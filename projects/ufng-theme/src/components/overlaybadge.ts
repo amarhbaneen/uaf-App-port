5d0cf2f89d3fa998cb3f80c84f69b689afd62bad
YSiri <yehiels14@gmail.com>
2025-07-16 15:01:00 +0300

    add ufng-theme project for packaging

---
+++ projects/ufng-theme/src/components/overlaybadge.ts
@@ -0,0 +1,10 @@
+import type { OverlayBadgeDesignTokens } from '@primeng/themes/types/overlaybadge';
+
+ export default {
+    root: {
+        outline: {
+            width: "2px",
+            color: "{content.background}"
+        }
+    }
+} satisfies OverlayBadgeDesignTokens;
